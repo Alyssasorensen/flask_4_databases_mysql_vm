2e13cc5f4397839a4ff2b372c15230da2d65d21b
Alyssasorensen <-m>
2023-10-06 00:42:47 +0000

    updated the README.md file

--- create.sql
+++ create.sql
@@ -18,26 +18,3 @@ CREATE TABLE patient_medications (
     FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
     FOREIGN KEY (medication_id) REFERENCES medications(medication_id)
 );
-
-This will create a clinical database with three tables: patients, medications, and patient_medications. Additionally, there is a foreign key relationship between the patient_medications table and both the patients and medications tables, which is used to link patients with the medications they are prescribed.
-
-The Schema I Chose and Why:
-
-Database Schema:
-
-1.  patients Table:
-    patient_id (Primary Key, automatic): A unique identifier for each patient.
-    first_name and last_name: Store the patient's first and last names.
-    date_of_birth: Records the patient's date of birth.
-
-2.  medications Table:
-    medication_id (Primary Key, automatic): A unique identifier for each medication.
-    medication_name: Stores the name of the medication.
-
-3.  patient_medications Table:
-    patient_medication_id (Primary Key, automatic): A unique identifier for each patient-medication relationship.
-    patient_id (Foreign Key, references patients(patient_id)): Links the patient to their prescribed medications.
-    medication_id (Foreign Key, references medications(medication_id)): Links the medication to the patients.
-    prescribed_date: Records the date when the medication was prescribed to the patient.
-
-My schema allows me to associate patients with the medications they are prescribed. The patient_id and medication_id fields in the patient_medications table create a foreign key relationship with the patients and medications tables, respectively, establishing the connection between patients and their prescribed medications.
